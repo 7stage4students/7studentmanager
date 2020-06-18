938f3780f0050a42a39f73419d94506b41dca27e
tekerene <tekerene@gmail.com>
2020-06-18 01:47:29 +0100

    registration and login form added

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 const express = require('express');
 const exphbs =  require('express-handlebars');
-
+const path = require('path');
+const { parseTwoDigitYear } = require('moment');
+const bodyParser = require('body-parser')
 const app = express()
-
+app.use(bodyParser.urlencoded({extended:true}))
 app.engine('handlebars',exphbs())
 app.set('view engine','handlebars');
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/',(req, res)=>{
     res.render('index');
 })
+app.get('/register', (req, res)=>{
+    res.render('register');
+});
+app.get('/login', (req, res) => {
+    res.render('login');
+});
 
 app.listen(8080,()=>{
     console.log('Server is running on Port 8080');
