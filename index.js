f0738fd8531b0179f38de234bb6cff546a76f383
bansiren <bansir61@gmail.com>
2020-06-19 10:14:47 +0100

    created-admin-login

--- index.js
+++ index.js
@@ -19,6 +19,12 @@ app.get('/register', (req, res)=>{
 app.get('/login', (req, res) => {
     res.render('login');
 });
+app.get('/admin_login', (req, res)=>{
+    res.render('admin_login')
+});
+app.get('/admin_landing', (req, res)=>{
+    res.render('admin_landing')
+})
 
 app.listen(8080,()=>{
     console.log('Server is running on http://127.0.0.1:8080');
